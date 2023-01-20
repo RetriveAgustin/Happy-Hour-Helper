3ca16aa1ff63c6d5766d426932d2229a0ea5f7e6
Ramiro Aduviri <95056360+Ramir095@users.noreply.github.com>
2023-01-20 09:15:21 -0300

    Footer (#11)
    * client structure + styled components
    
    * client structure + styled components
    
    * LandingScreen created
    
    * LandingScreen merge client structure + styled components
    
    * Footer created
    
    Co-authored-by: JotaCeV <jcvetromile@gmail.com>

---
+++ client/src/components/Header/index.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HeaderStyles = styled.div`
+    background-color: #52373C;
+    height: 50px;
+    width: 100%;
+    position: fixed;
+`
---
+++ client/src/views/LandingScreen/index.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { ImageBackground, Background } from "./index.styles";
+import response from "./data.json";
+
+import { Header } from "../../components/Header";
+import { SectionContainer } from "../../components/SectionContainer";
+import { Footer } from "../../components/Footer";
+
+export const LandingScreen = () => {
+  const [data, setData] = useState();
+
+  const getData = async () => {
+    setTimeout(() => {
+      setData(response);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <Background>
+      <Header />
+      <ImageBackground>
+        <h1>Bienvenido a Happy Hour Helper</h1>
+      </ImageBackground>
+      {data &&
+        data.map(d => {
+          return <SectionContainer key={d.title} title={d.title} data={d.data}/>;
+        })}
+      <Footer />
+    </Background>
+  );
+};
