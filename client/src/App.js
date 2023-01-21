2e8e2e866592711da9debcfea76655c1ac486d36
RetriveAgustin <retrive.agustin@gmail.com>
2023-01-20 23:33:19 -0300

    feat: routes established (views)

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "./Views/LandingPage/LandingPage";
-import CreateProduct from "./components/CreateProduct/CreateProduct";
+// import LandingPage from "./Views/LandingPage/LandingPage";
+// import CreateProduct from "./components/CreateProduct/CreateProduct";
 
 function App() {
+  //estos componentes son los views, a partir de ellos se van a presentar distintos componentes acorde a las demandas particulares del cliente.
+  //hay que proteger la ruta /user para que los usuarios no puedan ingresar
+  //para los links no validos se puede desarrollar un componente de error 404, o redireccionar al home.
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/form" element={<CreateProduct />} />
+      <Route path="/" element={<h1>LANDING PAGE</h1>} />
+      <Route path="/home" element={<h1>HOME</h1>} />
+      <Route path="/cart" element={<h1>CART</h1>} />
+      <Route path="/admin" element={<h1>ADMIN</h1>} />
+      <Route path="/user" element={<h1>USER</h1>} />
+      <Route path="/login" element={<h1>LOGIN</h1>} />
+      <Route path="/register" element={<h1>REGISTER</h1>} />
+      <Route path="*" element={<h1>404 PAGE NOT FOUND</h1>} />
     </Routes>
   );
 }
