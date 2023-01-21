d41aca4795861d2db750e71a7a30ccff0fa0d1ad
RetriveAgustin <retrive.agustin@gmail.com>
2023-01-21 10:49:45 -0300

    fix: small fixes

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,10 @@ function App() {
   //estos componentes son los views, a partir de ellos se van a presentar distintos componentes acorde a las demandas particulares del cliente.
   //hay que proteger la ruta /user para que los usuarios no puedan ingresar
   //para los links no validos se puede desarrollar un componente de error 404, o redireccionar al home.
+  //los componentes Navbar y Footer son componentes layout, por ende deben aparecer en todos los views.
   return (
     <Routes>
-      <Route path="/" element={<h1>LANDING PAGE</h1>} />
-      <Route path="/home" element={<h1>HOME</h1>} />
+      <Route path="/" element={<h1>HOME</h1>} />
       <Route path="/cart" element={<h1>CART</h1>} />
       <Route path="/admin" element={<h1>ADMIN</h1>} />
       <Route path="/user" element={<h1>USER</h1>} />
--- client/src/Views/LandingPage/LandingPage.jsx
+++ client/src/Views/LandingPage/LandingPage.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
-import { Card } from '../../components/Card/Card'
+import { Card } from "../../components/Card/Card";
 
 const LandingPage = () => {
   return (
     <>
-      <Link to='/form'>
+      <Link to="/form">
         <button>Create</button>
       </Link>
       <Card />
     </>
+  );
+};
 
 export default LandingPage;
