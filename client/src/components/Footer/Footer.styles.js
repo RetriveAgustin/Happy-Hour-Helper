bb8fc23f8ece984a734eb6da3645c8d2c71ff265
Ramiro Aduviri <95056360+Ramir095@users.noreply.github.com>
2023-01-21 18:24:52 -0300

    Feature/created home (#21)
    * fix: LandingPage error
    
    * Cambié el componente LandingPage por Home y arregle estilos rotos en el footer
    
    Co-authored-by: Nomad <96030392+RetriveAgustin@users.noreply.github.com>

--- client/src/components/Footer/Footer.styles.js
+++ client/src/components/Footer/Footer.styles.js
@@ -12,7 +12,8 @@ export const Container = styled.div`
   max-width: 1200px;
   margin: auto;
   grid-template-columns: repeat(4, 1fr);
-  padding: 20px;
+  padding: 30px 20px;
+  align-items: flex-start;
   gap: 80px;
 `;
 
@@ -20,18 +21,26 @@ export const HappyHourHelper = styled.div`
   /* background-color: yellow; */
   & p:first-child {
     margin: 0;
-    padding: 17px 0px;
+    /* padding: 17px 0px; */
     font-size: 1rem;
   }
+  padding-bottom: 10px;
   color: #eeecdf;
   width: 360px;
   text-align: start;
   font-size: 12px;
+  & p {
+    padding-bottom: 10px;
+  }
+  & p:last-child {
+    padding-top: 22px;
+  }
 `;
 
 export const Contact = styled.div`
   & p:first-child {
     font-size: 1rem;
+    padding-bottom: 6px;
   }
   /* background: blue; */
   color: #eeecdf;
@@ -39,6 +48,8 @@ export const Contact = styled.div`
   font-size: 12px;
   & a {
     display: flex;
+    /* background-color: red; */
+    padding: 5px 0;
     & img {
       width: 20px;
       padding-right: 10px;
@@ -50,6 +61,9 @@ export const Navigation = styled.div`
   /* background-color: salmon; */
   color: #eeecdf;
   text-align: start;
+  & p {
+    padding-bottom: 5px;
+  }
   & ul {
     font-size: 19px;
     padding: 0;
@@ -68,7 +82,6 @@ export const PaymentMethods = styled.div`
   align-items: flex-end;
   justify-content: start;
   & p {
-    margin: 16px 0;
     color: #eeecdf;
     font-size: 1rem;
     padding-bottom: 22px;
