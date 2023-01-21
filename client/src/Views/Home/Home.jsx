328a7b2b8ebfc643f266db66016e336acdefe93f
RetriveAgustin <retrive.agustin@gmail.com>
2023-01-21 20:20:36 -0300

    fix: logic in Home component commented

--- client/src/Views/Home/Home.jsx
+++ client/src/Views/Home/Home.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import { ImageBackground, Background } from "./Home.styles";
 
-import { Header } from "../../components/Header/Header";
+import Header from "../../components/Header/Header";
 import { Section } from "../../components/Section/Section";
 import { Footer } from "../../components/Footer/Footer";
 
 const Home = () => {
-  const [data, setData] = useState();
+  // const [data, setData] = useState();
 
   // const getData = async () => {
   //   setTimeout(() => {
@@ -24,10 +24,10 @@ const Home = () => {
       <ImageBackground>
         <h1>Bienvenido a Happy Hour Helper</h1>
       </ImageBackground>
-      {data &&
+      {/* {data &&
         data.map(d => {
           return <Section key={d.title} title={d.title} data={d.data}/>;
-        })}
+        })} */}
       <Footer />
     </Background>
   );
