2c5537ab0e0e46dbeb1141262ad6bd5042c145ae
Nomad <96030392+RetriveAgustin@users.noreply.github.com>
2023-01-23 17:11:53 -0300

    Agustin (#28)
    * feat: componente Login y Register añadidos
    
    * Bugs solucionados
    
    * feature/FilterCreated
    
    * fix:Home
    
    * filters fixed
    
    * feat: rel created between Offer and Category
    
    Co-authored-by: Loloming <damianbruzzoni@gmail.com>
    Co-authored-by: Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>

--- api/src/routes/controllers/productController.js
+++ api/src/routes/controllers/productController.js
@@ -69,14 +69,17 @@ const postProduct = async (req, res) => {
   }
 };
 
+//se cambió el medio de obtención de las variables, ahora se mandan por body.
+//además ahora se recibe un id, no un nombre, puesto que el producto no tiene acceso a esta información
+//en el home se tendrá que modificar esto para que funcione adecuadamente
 const filterProductByBrand = async (req, res) => {
   try {
-    const { brand } = req.query;
+    const { brand } = req.body;
     Product.findAll({
       where: {
         brand,
       },
-      include: [{ model: Brand, where: { name: brand } }],
+      include: [{ model: Brand, where: { id: brand } }],
     }).then((products) => res.status(200).json(products));
   } catch (error) {
     res.status(400).error.message;
@@ -85,12 +88,12 @@ const filterProductByBrand = async (req, res) => {
 
 const filterProductByCategory = async (req, res) => {
   try {
-    const { category } = req.query;
+    const { category } = req.body;
     Product.findAll({
       where: {
         category,
       },
-      include: [{ model: Category, where: { name: category } }],
+      include: [{ model: Category, where: { id: category } }],
     }).then((products) => res.status(200).json(products));
   } catch (error) {
     res.status(400).error.message;
@@ -98,12 +101,12 @@ const filterProductByCategory = async (req, res) => {
 };
 
 const filterProductBySubCategory = async (req, res) => {
-  const { subcategory } = req.query;
+  const { subcategory } = req.body;
   Product.findAll({
     where: {
       subcategory: subcategory,
     },
-    include: [{ model: Sub_category, where: { name: subcategory } }],
+    include: [{ model: Sub_category, where: { id: subcategory } }],
   }).then((products) => res.json(products));
 };
 
--- client/src/Views/Home/Home.jsx
+++ client/src/Views/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-
 import {
   ImageBackground,
   Background,
