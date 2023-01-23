1efe88d1a182573a78ac7cba0a57f7433dc7e84f
RetriveAgustin <retrive.agustin@gmail.com>
2023-01-23 18:00:19 -0300

    restored branch main

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-
-
-conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); 
-  });
-});
