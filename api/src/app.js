aab422406825adefe9b98d998756800b47a01801
RetriveAgustin <retrive.agustin@gmail.com>
2023-01-29 20:00:43 -0300

    cors changed

--- api/src/app.js
+++ api/src/app.js
@@ -5,9 +5,6 @@ const morgan = require('morgan');
 const routes= require('./routes/index.js');
 const cors = require('cors')
 require('dotenv').config();
-const {
-  FRONTEND_HOST
-} = process.env;
 
 require('./db.js');
 
@@ -20,13 +17,6 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(cors());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "http://localhost:3000/"); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
 
 server.use('/', routes);
 
