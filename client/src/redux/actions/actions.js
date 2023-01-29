b8179164e1df5da79acbcc00982ed1d2eae5e6e2
JotaCeV <jcvetromile@gmail.com>
2023-01-28 22:04:46 -0300

    el heroe sin capa

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -12,7 +12,7 @@ export const ADD_TO_CART = "ADD_TO_CART";
 
 export const getAllCategories = () => {
   return function (dispatch) {
-    fetch(`${process.env.REACT_APP_API_URL}/category/getCategory`)
+    fetch(`${process.env.REACT_APP_API_URL}category/getCategory`)
       .then((response) => response.json())
       .then((data) => {
         dispatch({
@@ -26,7 +26,7 @@ export const getAllCategories = () => {
 
 export const getAllSubCategories = () => {
   return function (dispatch) {
-    fetch(`${process.env.REACT_APP_API_URL}/sub-category/getSubCategory`)
+    fetch(`${process.env.REACT_APP_API_URL}sub-category/getSubCategory`)
       .then((response) => response.json())
       .then((data) => {
         dispatch({
@@ -40,7 +40,7 @@ export const getAllSubCategories = () => {
 
 export const getAllProducts = () => {
   return function (dispatch) {
-    fetch(`${process.env.REACT_APP_API_URL}/products/getProduct`)
+    fetch(`${process.env.REACT_APP_API_URL}products/getProduct`)
       .then((response) => response.json())
       .then((data) => {
         dispatch({
@@ -54,7 +54,7 @@ export const getAllProducts = () => {
 
 export const getProductId = (payload) => {
   return function (dispatch) {
-    fetch(`${process.env.REACT_APP_API_URL}/getProductId/${payload}`)
+    fetch(`${process.env.REACT_APP_API_URL}getProductId/${payload}`)
       .then((response) => response.json())
       .then((data) => {
         dispatch({
@@ -68,7 +68,7 @@ export const getProductId = (payload) => {
 
 export const getAllBrands = () => {
   return function (dispatch) {
-    fetch(`${process.env.REACT_APP_API_URL}/brand/getBrand`)
+    fetch(`${process.env.REACT_APP_API_URL}brand/getBrand`)
       .then((response) => response.json())
       .then((data) => {
         dispatch({
@@ -82,7 +82,7 @@ export const getAllBrands = () => {
 
 export const getAllOffers = () => {
   return function (dispatch) {
-    fetch(`${process.env.REACT_APP_API_URL}/getOffer`)
+    fetch(`${process.env.REACT_APP_API_URL}getOffer`)
       .then((response) => response.json())
       .then((data) => {
         dispatch({
@@ -96,14 +96,20 @@ export const getAllOffers = () => {
 
 export const createCategory = (payload) => {
   return async function () {
-    const post = await axios.post(`${process.env.REACT_APP_API_URL}/category`, payload);
+    const post = await axios.post(
+      `${process.env.REACT_APP_API_URL}category`,
+      payload
+    );
     return post;
   };
 };
 
 export const createProduct = (payload) => {
   return async function () {
-    const post = await axios.post(`${process.env.REACT_APP_API_URL}/products`, payload);
+    const post = await axios.post(
+      `${process.env.REACT_APP_API_URL}products`,
+      payload
+    );
     return post;
   };
 };
@@ -111,7 +117,7 @@ export const createProduct = (payload) => {
 export const createSubCategory = (payload) => {
   return async function () {
     const post = await axios.post(
-    `${process.env.REACT_APP_API_URL}/sub-category`,
+      `${process.env.REACT_APP_API_URL}sub-category`,
       payload
     );
     return post;
@@ -120,26 +126,32 @@ export const createSubCategory = (payload) => {
 
 export const createOffer = (payload) => {
   return async function () {
-    const post = await axios.post(`${process.env.REACT_APP_API_URL}/offer`, payload);
+    const post = await axios.post(
+      `${process.env.REACT_APP_API_URL}offer`,
+      payload
+    );
     return post;
   };
 };
 
 export const createBrand = (payload) => {
   return async function () {
-    const post = await axios.post(`${process.env.REACT_APP_API_URL}/brand`, payload);
+    const post = await axios.post(
+      `${process.env.REACT_APP_API_URL}brand`,
+      payload
+    );
     return post;
   };
 };
 
 export const addToCart = (payload) => {
   return function (dispatch) {
-    fetch(`${process.env.REACT_APP_API_URL}/products/getProductId/${payload}`)
-    .then((response) => response.json())
+    fetch(`${process.env.REACT_APP_API_URL}products/getProductId/${payload}`)
+      .then((response) => response.json())
       .then((data) => {
         dispatch({
           type: ADD_TO_CART,
-          payload: data
+          payload: data,
         });
       })
       .catch((err) => console.log(err));
